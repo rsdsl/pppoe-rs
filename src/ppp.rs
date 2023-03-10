ca4afe766bf8366da16864eef0eec7c0d7c1589e
HimbeerserverDE <himbeerserverde@gmail.com>
2023-03-10 17:20:33 +0100

    add ppp header builders

--- src/ppp.rs
+++ src/ppp.rs
@@ -2,6 +2,18 @@ use byteorder::{ByteOrder, NetworkEndian as NE};
 
 use crate::error::ParseError;
 
+pub const LCP: u16 = 0xc021;
+pub const PAP: u16 = 0xc023;
+pub const CHAP: u16 = 0xc223;
+
+#[repr(u16)]
+#[derive(PartialEq, Eq, Copy, Clone)]
+pub enum Protocol {
+    Lcp = LCP,
+    Pap = PAP,
+    Chap = CHAP,
+}
+
 fn ensure_minimal_buffer_length(buffer: &[u8]) -> Result<(), ParseError> {
     if buffer.len() < 2 {
         return Err(ParseError::BufferTooSmall(buffer.len()));
@@ -10,9 +22,9 @@ fn ensure_minimal_buffer_length(buffer: &[u8]) -> Result<(), ParseError> {
 }
 
 #[derive(Debug)]
-pub struct PppHeader<'a>(&'a [u8]);
+pub struct Header<'a>(&'a [u8]);
 
-impl<'a> PppHeader<'a> {
+impl<'a> Header<'a> {
     pub fn with_buffer(buffer: &'a [u8]) -> Result<Self, ParseError> {
         ensure_minimal_buffer_length(buffer)?;
         if buffer[0] & 0x01 != 0 || buffer[1] & 0x80 != 0x80 {
@@ -20,7 +32,7 @@ impl<'a> PppHeader<'a> {
             return Err(ParseError::InvalidPppProtocol(protocol));
         }
 
-        Ok(PppHeader(buffer))
+        Ok(Header(buffer))
     }
 
     pub fn as_bytes(&self) -> &[u8] {
@@ -45,5 +57,45 @@ impl<'a> PppHeader<'a> {
 
     pub fn payload(&self) -> &[u8] {
         &self.0[2..]
+    }
+}
+
+pub struct HeaderBuilder<'a>(&'a mut [u8]);
+
+impl<'a> HeaderBuilder<'a> {
+    pub fn protocol(&self) -> u16 {
+        NE::read_u16(&self.0[..2])
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 2
+    }
+
+    pub fn payload(&self) -> &[u8] {
+        &self.0[2..]
+    }
+
+    pub fn set_protocol(&mut self, protocol: Protocol) {
+        NE::write_u16(&mut self.0[..2], protocol as u16);
+    }
+
+    pub fn create_packet(buffer: &'a mut [u8], protocol: Protocol) -> Result<Self, ParseError> {
+        ensure_minimal_buffer_length(buffer)?;
+
+        NE::write_u16(&mut buffer[..2], protocol as u16);
+
+        Ok(HeaderBuilder(buffer))
+    }
+
+    pub fn get_ref_mut(&mut self) -> &mut [u8] {
+        self.0
+    }
+
+    pub fn build(self) -> Result<Header<'a>, ParseError> {
+        Header::with_buffer(self.0)
     }
 }
